cfdfb18537b472c52f50d56dce7fff058813651d
cgb14 <844386320@qq.com>
2016-07-20 16:02:36 +0800

    first commit

---
+++ weixinInterface.py
@@ -0,0 +1,31 @@
+import hashlib
+import web
+import lxml
+import time
+import os
+
+class WeixinInterface:
+	def __init__(self):
+		self.app_root = os.path.dirname(__file__)
+		self.templates_root = os.path.join(self.app_root,'templates)
+		self.render = web.template.render(self.templates_root)
+	def GET(self):
+		#获取输入参数
+		data = web.input()
+		signature = data.signature
+		timestamp = data.timestamp
+		nonce = data.nonce
+		echostr = data.echostr
+		#自己的token
+		token = "cgb157dnfcf953"
+		#字典序排序
+		list = [token,timestamp,nonce]
+		list.sort()
+		sha1 = hashlib.sha1()
+		map(sha1.update,list)
+		hashcode = sha1.hexdigest()
+		#sha1加密算法
+		
+		#如果是来自微信的请求，则回复echostr
+		if hashcode == signature:
+			return echostr
