2127922dd1511c2fd7ed589d8e47b176d71a642a
cgb14 <844386320@qq.com>
2016-07-20 22:04:29 +0800

    return the same message 4

--- weixinInterface.py
+++ weixinInterface.py
@@ -8,6 +8,7 @@ import os
 
 class WeixinInterface:
 	def __init__(self):
+		print 'fuck'
 		self.app_root = os.path.dirname(__file__)
 		self.templates_root = os.path.join(self.app_root,'templates')
 		self.render = web.template.render(self.templates_root)
