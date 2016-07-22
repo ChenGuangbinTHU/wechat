82c0c6decfc926e924906d593eb53d6aa0ba7621
cgb14 <844386320@qq.com>
2016-07-22 15:36:26 +0800

    7.22 1

--- weixinInterface.py
+++ weixinInterface.py
@@ -37,7 +37,7 @@ class WeixinInterface:
 			
 	def POST(self): 
 		str_xml = web.data() #获得post来的数据 
-		xml = etree.fromstring(str_xml)#进行XML解析 
+		xml = lxml.etree.fromstring(str_xml)#进行XML解析 
 		msgType=xml.find("MsgType").text 
 		fromUser=xml.find("FromUserName").text 
 		toUser=xml.find("ToUserName").text 
