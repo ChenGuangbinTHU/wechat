2ee59063bc9ade7b4405a9b87a55f1f48b52c349
cgb14 <844386320@qq.com>
2016-07-22 16:10:21 +0800

    7.21 5

--- weixinInterface.py
+++ weixinInterface.py
@@ -15,29 +15,27 @@ class WeixinInterface:
 		self.render = web.template.render(self.templates_root)
 
 	def GET(self):
-		#获取输入参数
+		
 		data = web.input()
 		signature=data.signature
 		timestamp=data.timestamp
 		nonce=data.nonce
 		echostr = data.echostr
-		#自己的token
-		token="cgb157dnfcf953" #这里改写你在微信公众平台里输入的token
-		#字典序排序
+		
+		token="cgb157dnfcf953" 
+		
 		list=[token,timestamp,nonce]
 		list.sort()
 		sha1=hashlib.sha1()
 		map(sha1.update,list)
 		hashcode=sha1.hexdigest()
-		#sha1加密算法
-
-		#如果是来自微信的请求，则回复echostr
+		
 		if hashcode == signature:
 			return echostr
 			
 	def POST(self): 
-		str_xml = eb.data() #获得post来的数据 
-		xml = lxml.etree.fromstring(str_xml)#进行XML解析 
+		str_xml = web.data() 
+		xml = lxml.etree.fromstring(str_xml)
 		msgType = xml.find("MsgType").text 
 		fromUser=xml.find("FromUserName").text 
 		toUser=xml.find("ToUserName").text 
